4f347b2accc684d2fe46da6040790caaaa4e61aa
Ekaterina Shepeleva <ecganshina@gmail.com>
2025-10-31 01:25:12 +0300

    Added Typescript to project

--- src/components/App/App.jsx
+++
@@ -1,9 +0,0 @@
-import './App.css'
-
-function App() {
-  return (
-      <div>Boilerplate is ready</div>
-  )
-}
-
-export default App
---
+++ src/components/App/App.tsx
@@ -0,0 +1,9 @@
+import './App.css';
+
+const App = () => {
+    return (
+        <div>Boilerplate is ready</div>
+    )
+}
+
+export default App;
