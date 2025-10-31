41430eb44e8629d56bc852579933d249fec24a39
Ekaterina Shepeleva <ecganshina@gmail.com>
2025-10-31 16:29:31 +0300

    Added rules of the game

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,22 @@
 import './App.css';
 import Board from 'components/Board/Board';
+import Rules from 'components/Rules/Rules';
 
 const App = () => {
     return (
-        <>
-            <Board/>
-        </>
+        <div className="container">
+            <h1>Rock, paper, scissors</h1>
+
+            <div className="wrapper">
+                <main className="board-container">
+                    <Board/>
+                </main>
+
+                <aside className="aside">
+                    <Rules/>
+                </aside>
+            </div>
+        </div>
     );
 };
 
---
+++ src/components/Rules/Rules.tsx
@@ -0,0 +1,15 @@
+const Rules = () => (
+    <div className="rules">
+        <h3>Rules of the game</h3>
+        <p>
+            An easily memorised rule determines the winner:
+            “Rock breaks scissors, scissors cuts paper, paper covers rock.”
+        </p>
+        <p>
+            In other words, a player who chooses rock beats one who chooses scissors; scissors in turn beats paper;
+            paper beats rock. This yields a winner whenever the two players choose differently.
+        </p>
+    </div>
+);
+
+export default Rules;
